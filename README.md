a458e9cc1f30a25f7c360fc806deba6b06de3628
François <francois.mockers@vleue.com>
2024-04-26 02:16:38 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Bevy Easings
+
+![MIT/Apache 2.0](https://img.shields.io/badge/license-MIT%2FApache-blue.svg)
+[![Doc](https://docs.rs/vleue_sentry/badge.svg)](https://docs.rs/vleue_sentry)
+[![Crate](https://img.shields.io/crates/v/vleue_sentry.svg)](https://crates.io/crates/vleue_sentry)
+[![Bevy Tracking](https://img.shields.io/badge/Bevy%20tracking-main-lightblue)](https://github.com/bevyengine/bevy/blob/main/docs/plugins_guidelines.md#main-branch-tracking)
+[![CI](https://github.com/vleue/vleue_sentry/actions/workflows/ci.yml/badge.svg)](https://github.com/vleue/vleue_sentry/actions/workflows/ci.yml)
+
+Error reporting for Bevy using [Sentry](https://sentry.io).
+
+## Usage
+
+Set the login subscriber:
+
+```rust
+use bevy::{prelude::*, log::LogPlugin};
+
+use vleue_sentry::sentry_panic_reporter;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins.set(LogPlugin {
+            update_subscriber: Some(sentry_panic_reporter),
+            ..default()
+        }));
+}
+```
---
+++ src/lib.rs
@@ -0,0 +1,111 @@
+#![warn(
+    missing_copy_implementations,
+    trivial_casts,
+    trivial_numeric_casts,
+    unsafe_code,
+    unstable_features,
+    unused_import_braces,
+    unused_qualifications,
+    missing_docs
+)]
+#![doc = include_str!("../README.md")]
+
+use std::env;
+
+use bevy::log::BoxedSubscriber;
+use tracing::{Level, Subscriber};
+use tracing_subscriber::{layer::SubscriberExt, Layer};
+
+use sentry::ClientInitGuard;
+
+struct SentryLayer {
+    #[allow(dead_code)]
+    guard: ClientInitGuard,
+    report_only_panic: bool,
+}
+
+impl<S: Subscriber> Layer<S> for SentryLayer {
+    fn on_event(
+        &self,
+        event: &tracing::Event<'_>,
+        _ctx: tracing_subscriber::layer::Context<'_, S>,
+    ) {
+        let breadcrumb = sentry_tracing::breadcrumb_from_event(event);
+        if event.metadata().level() == &Level::ERROR && !self.report_only_panic {
+            sentry::capture_event(sentry::protocol::Event {
+                level: breadcrumb.level,
+                message: breadcrumb.message,
+                timestamp: breadcrumb.timestamp,
+                ..Default::default()
+            });
+        } else {
+            sentry::add_breadcrumb(breadcrumb);
+        }
+    }
+}
+
+/// Reports panics to Sentry
+/// logs will be added as breadcrumbs
+///
+/// ```rust
+/// # use bevy::prelude::*;
+/// # use bevy::log::LogPlugin;
+/// use vleue_sentry::sentry_panic_reporter;
+///
+/// App::new()
+///     .add_plugins(DefaultPlugins.set(LogPlugin {
+///         update_subscriber: Some(sentry_panic_reporter),
+///         ..default()
+///     }));
+/// ```
+pub fn sentry_panic_reporter(subscriber: BoxedSubscriber) -> BoxedSubscriber {
+    if let Ok(sentry_dsn) = env::var("SENTRY_DSN") {
+        let guard = sentry::init((
+            sentry_dsn,
+            sentry::ClientOptions {
+                release: sentry::release_name!(),
+                ..Default::default()
+            },
+        ));
+
+        Box::new(subscriber.with(SentryLayer {
+            guard,
+            report_only_panic: true,
+        }))
+    } else {
+        subscriber
+    }
+}
+
+/// Reports panics and errors to Sentry
+/// logs will be added as breadcrumbs
+///
+/// ```rust
+/// # use bevy::prelude::*;
+/// # use bevy::log::LogPlugin;
+/// use vleue_sentry::sentry_error_reporter;
+///
+/// App::new()
+///     .add_plugins(DefaultPlugins.set(LogPlugin {
+///         update_subscriber: Some(sentry_error_reporter),
+///         ..default()
+///     }));
+/// ```
+pub fn sentry_error_reporter(subscriber: BoxedSubscriber) -> BoxedSubscriber {
+    if let Ok(sentry_dsn) = env::var("SENTRY_DSN") {
+        let guard = sentry::init((
+            sentry_dsn,
+            sentry::ClientOptions {
+                release: sentry::release_name!(),
+                ..Default::default()
+            },
+        ));
+
+        Box::new(subscriber.with(SentryLayer {
+            guard,
+            report_only_panic: false,
+        }))
+    } else {
+        subscriber
+    }
+}
